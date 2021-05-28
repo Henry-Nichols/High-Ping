ddd79d85add656e6da5cc7332f19ef09a0084b7b
Henry Nichols <nicholsh562@gmail.com>
2021-05-28 04:43:37 +0000

    Completed-Hamburger-Menu

--- client/navbar.jsx
+++ client/navbar.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
-import  Menu  from './Menu'
+import Menu from './Menu';
 
 export default class Navbar extends React.Component {
+  state = { clicked: false }
+
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  }
+
   render() {
     return (
       <nav className="navbar">
         <h1 className="name-logo">High Ping</h1>
-        <div className="menu-icons">
-          <i className="fas fa-bars"></i>
+        <div className="menu-icons" onClick={ this.handleClick }>
+          <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
         </div>
-        <ul>
-          { Menu.map((item, index) => {
+        <ul className={ this.state.clicked ? 'nav-menu clicked' : 'nav-menu'}>
+          { Menu.map(item => {
             return (
               <li key={ item.id }>
                 <a className={ item.cName } href={ item.url }>
                   { item.title }
                 </a>
               </li>
-            )
+            );
           })}
         </ul>
       </nav>
